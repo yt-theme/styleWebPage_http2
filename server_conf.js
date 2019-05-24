ac8403e5483473617a1709a571638e7d56efd7c3
yt <yt@192.168.0.101>
2019-05-24 21:10:50 +0800

    20190524

--- server_conf.js
+++ server_conf.js
@@ -1,5 +1,8 @@
-const Server_port = 6900
-
+// 配置文件
 module.exports = {
-    Server_port: Server_port
+    // 服务器 端口
+    Server_port           : 6900,
+    // 服务器 私钥 公钥
+    Server_privateKey_src : __dirname + '/src/server/key/server.key',
+    Server_publicKey_src  : __dirname + '/src/server/key/server.crt' 
 }
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,8 @@
-const http2           = require('http2')
-const fs              = require('fs')
+const http2 = require('http2')
+const fs    = require('fs')
 
 // 配置文件
-const { Server_port } = require('../../server_conf')
+const { Server_port, Server_privateKey_src, Server_publicKey_src } = require('../../server_conf')
 
 // 服务器 启动 方法
 class Server {
@@ -14,18 +14,24 @@ class Server {
         this.http2_server = null
 
         // 私钥
-        this.private_key = fs.readFileSync('./key/server.key')
+        this.private_key = fs.readFileSync(Server_privateKey_src)
         // 公钥
-        this.public_key  = fs.readFileSync('./key/server.crt')
+        this.public_key  = fs.readFileSync(Server_publicKey_src)
     }
 
     // 启动
     start () {
         console.log('server start on =>', Server_port)
-        this.http2_server = http2.createSecureServer({
-            key: this.private_key,
-            cert: this.public_key
+        this.http2_server = http2.createSecureServer({ key: this.private_key, cert: this.public_key })
+        this.http2_server.on('error',  (err) => { console.log('server start err =>', err) })
+        this.http2_server.on('stream', (stream, headers) => {
+            stream.respond({
+                'content-type': 'text-html',
+                ':status': 200
+            })
+            stream.end('<h2>hello world</h2>')
         })
+        this.http2_server.listen(Server_port)
     }
 }
 
